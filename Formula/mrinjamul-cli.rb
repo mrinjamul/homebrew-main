f4f4e988cce94e0196664ab3b4129b67240499a3
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-22 01:23:20 +0530

    Brew formula update for mrinjamul-cli version v1.1.1

--- Formula/mrinjamul-cli.rb
+++ Formula/mrinjamul-cli.rb
@@ -5,20 +5,20 @@
 class MrinjamulCli < Formula
   desc "A CLI to sync domains from local to Cloudflare."
   homepage "https://github.com/linuxuserin/go-github"
-  version "1.1.1-beta"
+  version "1.1.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/mrinjamul/mrinjamul-cli/releases/download/v1.1.1-beta/mrinjamul-cli_1.1.1-beta_darwin_amd64.tar.gz"
-      sha256 "95657a85c517a743e71e69fddca50d6c4a206640a76d97f76b7d374463969e6b"
+      url "https://github.com/mrinjamul/mrinjamul-cli/releases/download/v1.1.1/mrinjamul-cli_1.1.1_darwin_amd64.tar.gz"
+      sha256 "0568f64e09d68f9b925c3d9b361fd5c71f4d42cf0ade0e68524451ccfa9a7d8e"
 
       def install
         bin.install "mrinjamul-cli"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/mrinjamul/mrinjamul-cli/releases/download/v1.1.1-beta/mrinjamul-cli_1.1.1-beta_darwin_arm64.tar.gz"
-      sha256 "645fc6dc407ed0c4522fa0d7b98bea7ce93448d6b07f94c3d6573275566d0c5e"
+      url "https://github.com/mrinjamul/mrinjamul-cli/releases/download/v1.1.1/mrinjamul-cli_1.1.1_darwin_arm64.tar.gz"
+      sha256 "4a1b4a594660b2cf463b0f5e06b5724ea2c510c0a366f3bafbf4c43aca3ed8f4"
 
       def install
         bin.install "mrinjamul-cli"
@@ -27,17 +27,17 @@ class MrinjamulCli < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/mrinjamul/mrinjamul-cli/releases/download/v1.1.1-beta/mrinjamul-cli_1.1.1-beta_linux_amd64.tar.gz"
-      sha256 "37924cce034fd2af280bd9c90c5e7aa081da0f366f73d333c80dcc6a0b4dbc64"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/mrinjamul/mrinjamul-cli/releases/download/v1.1.1/mrinjamul-cli_1.1.1_linux_arm64.tar.gz"
+      sha256 "d6e62710aab226b97e6b5db4d7ac98f3f36a40ac696f7fc6300330c3d0d34521"
 
       def install
         bin.install "mrinjamul-cli"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mrinjamul/mrinjamul-cli/releases/download/v1.1.1-beta/mrinjamul-cli_1.1.1-beta_linux_arm64.tar.gz"
-      sha256 "68d27de58e508f64a6edc3aced6946869dc32d0c51c41c89163d0515e21e2918"
+    if Hardware::CPU.intel?
+      url "https://github.com/mrinjamul/mrinjamul-cli/releases/download/v1.1.1/mrinjamul-cli_1.1.1_linux_amd64.tar.gz"
+      sha256 "a89f542f575fff3c66e4722ffa6dbad64fe272efbe082e37c12a35f0770e97a3"
 
       def install
         bin.install "mrinjamul-cli"
